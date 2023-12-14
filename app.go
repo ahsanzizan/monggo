8b0cc07fdc499062c20c5e9fc250f3ac354af965
Ahsan <ahsanaz461@gmail.com>
2023-12-14 09:37:19 +0700

    Initial commit

---
+++ app.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// User struct remains the same
+type User struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+var client *mongo.Client
+var collection *mongo.Collection
+
+func initMongoDB() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection = client.Database("monggo").Collection("users")
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	cur, err := collection.Find(context.Background(), bson.D{})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer cur.Close(context.Background())
+
+	var users []User
+	for cur.Next(context.Background()) {
+		var user User
+		err := cur.Decode(&user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, user)
+	}
+
+	if err := cur.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(users)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	id := r.URL.Query().Get("id")
+
+	idInt := 0
+	fmt.Sscanf(id, "%d", &idInt)
+
+	var user User
+	err := collection.FindOne(context.Background(), bson.M{"id": idInt}).Decode(&user)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func main() {
+	initMongoDB()
+
+	http.HandleFunc("/users", getUsers)
+	http.HandleFunc("/user", getUser)
+
+	fmt.Println("Server is running on :8080...")
+	http.ListenAndServe(":8080", nil)
+}
